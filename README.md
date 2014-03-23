4f8439cb78e379d113fbaed4d627b523357f01f3
Samuel Wejeus <wejeus@fastmail.fm>
2014-03-23 16:49:07 +0100

    GitPic version 1.0

---
+++ README.md
@@ -0,0 +1,23 @@
+GitPic
+=======
+
+http
+Make your git commit history more fun by automatically make a webcam capture for each commit you make! Captures will be saved and sorted based on project and time so you can easily browse your history. As a bonus the commit message is rendered to the captured image. Holy smokes thats awesome!
+
+## Install (Mac OSX)
+
+GitPic relies on Python and OpenCV (and its Python bindings of course). Easiest way to install required packages is to first install MacPorts (http://www.macports.org/) then from a commandline run:
+	
+	sudo port install opencv +python27 // this will take a while..
+
+To setup git allways use the GitPic hooks setup a template dir:
+
+	git config --global init.templatedir "~/.git_template"
+
+Then copy the files "capture_webcam.py", "post-commit" to this folder.
+
+Now every time you create a new git repository with 'git init' GitPic will automatically be installed! Whohoo! If you already have an existing git repository but still want all the GitPic goodness it is safe to run 'git init' again and git will copy any new hooks to your existing repository.
+
+## Configuration
+
+Images will by default be stored in ~/code/gitpic/ but you can change this by editing the constant "OUTPUT_DIR" in the script.
---
+++ capture_webcam.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+# Author: Samuel Wejeus (wejeus@fastmail.fm)
+
+import cv2
+import numpy as np
+import time
+import os
+import subprocess
+import sys
+
+# Configuration
+OUTPUT_DIR = "~/code/gitpic/"
+DEBUG = False
+
+def capture(cameraFeed, path, message):
+	fontFace = cv2.FONT_HERSHEY_SIMPLEX
+	fontScale = 1
+	thickness = 1;
+	padding = 10
+
+	# we need couple of  frames before camera has adjusted to light conditions
+	frame = None
+	for x in range(0, 10):
+		_, frame = cameraFeed.read()
+	
+	# add commit message
+	(_, textHeight), _ = cv2.getTextSize(message, fontFace, fontScale, thickness);
+	textPosY = textHeight
+	lines = message.splitlines()
+	for line in lines:
+		position = (padding, textPosY+padding)
+		# little hack to get a outline on text since opencv does not support it natively
+		cv2.putText(frame, line, position, fontFace, fontScale, (0, 0, 0), thickness+2, lineType = 0)
+		cv2.putText(frame, line, position, fontFace, fontScale, (255, 255, 255), thickness, lineType = cv2.CV_AA)
+		textPosY = textPosY + textHeight + padding
+	
+	# output final image
+	cv2.imwrite(path, frame)
+
+# Program ----------------------------------------------
+
+projectBasePath = subprocess.Popen(["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE).communicate()[0]
+commitMessage = subprocess.Popen(["git", "log", "--format=%B", "-n", "1 HEAD"], stdout=subprocess.PIPE).communicate()[0].strip()
+if DEBUG:
+	projectBasePath = os.path.dirname(os.path.abspath(__file__))
+	commitMessage = "Debug commit message\nspanning multiple\nlines\n=)"
+
+if projectBasePath == "" or commitMessage == "":
+	sys.exit(1)
+
+projectName = os.path.basename(projectBasePath).strip()
+uri = os.path.expanduser(OUTPUT_DIR + projectName + "/")
+
+# create dir if needed
+cmd_makeDirs = "mkdir -pv " + os.path.dirname(uri)
+ret = subprocess.call(cmd_makeDirs, shell=True)
+if ret != 0:
+    sys.exit(1)
+
+# prepare output filename
+filename = time.strftime("%d-%m-%Y-%H-%M") + ".png"
+imagePath = uri + filename
+
+if DEBUG:
+	print "gitpic: " + commitMessage + " -> " + imagePath
+
+# init opencv camera
+cameraFeed = cv2.VideoCapture(0)
+capture(cameraFeed, imagePath, commitMessage)
+
+
+
